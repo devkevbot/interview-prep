4ce89d2390086676e4aa11f16ac80c252c0aaca8
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2023-08-17 19:05:44 -0700

    problem: 0100

---
+++ easy-0100-same-tree.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        """
+        Time: O(n + m), where n is the number of nodes in p and m is the number of nodes in q
+        Space: O()
+        """
+        if not p and not q:
+            return True
+
+        if (p and not q) or (q and not p):
+            return False
+
+        return (
+            p.val == q.val
+            and self.isSameTree(p.left, q.left)
+            and self.isSameTree(p.right, q.right)
+        )
