91972404de906230557f4bc772ad0e206fad45f6
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2023-08-23 14:11:32 -0700

    problem: 0235

---
+++ medium-0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+# Recursive solution
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        """
+        Time: O(h), where h is the height of the tree
+        Space: O(h)
+        """
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
+
+
+# Iterative solution
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        """
+        Time: O(h), where h is the height of tree
+        Space: O(1)
+        """
+        curr = root
+        while curr:
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            else:
+                return curr
