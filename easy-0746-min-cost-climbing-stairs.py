5f8359c47b21dce533e45f61987f8f3211a7de4e
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2023-08-24 16:05:14 -0700

    refine: problem 0746

--- easy-0746-min-cost-climbing-stairs.py
+++ easy-0746-min-cost-climbing-stairs.py
@@ -60,12 +60,8 @@ class Solution:
         first = cost[0]
         second = cost[1]
 
-        if n < 2:
-            return min(first, second)
-
         for i in range(2, n):
             curr = cost[i] + min(first, second)
-            first = second
-            second = curr
+            first, second = second, curr
 
         return min(first, second)
