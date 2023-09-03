dbb57dcf92c6554c778b0ae4d44e0162cc572880
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2023-09-03 14:48:59 -0700

    refine: 0200

--- medium-0200-number-of-islands.py
+++ medium-0200-number-of-islands.py
@@ -6,36 +6,33 @@ class Solution:
     def num_islands(grid: list[list[str]]) -> int:
         """
         Given an m x n grid:
-        Time: O(m * n)
-        Space: O(m * n)
+        Time: O(mn)
+        Space: O(mn)
         """
-
-        def is_on_grid(row: int, col: int) -> bool:
-            row_in_bounds = row >= 0 and row < rows
-            col_in_bounds = col >= 0 and col < cols
-            return row_in_bounds and col_in_bounds
-
-        def is_land(row: int, col: int) -> bool:
-            if not is_on_grid(row, col):
-                return False
-            return grid[row][col] == "1"
 
         if len(grid) == 0 or len(grid[0]) == 0:
             return 0
 
+        num_islands = 0
         rows = len(grid)
         cols = len(grid[0])
-
-        number_of_islands = 0
-        directions = [[0, -1], [1, 0], [0, 1], [-1, 0]]
-        # If changing the input is allowed, we could also directly mark the grid squares with a number
-        # that would identify if the grid square has been seen before, improving space complexity.
+        # Rather than using a set, it's also possible to modify the grid in-place to signify a seen cell.
+        # Using that approach, space complexity can be reduced.
         seen = set()
+
+        def is_unvisited_land(row, col):
+            row_ok = row >= 0 and row < rows
+            col_ok = col >= 0 and col < cols
+            return (
+                row_ok and col_ok and (row, col) not in seen and grid[row][col] == "1"
+            )
+
+        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
 
         for row in range(rows):
             for col in range(cols):
-                if is_land(row, col) and (row, col) not in seen:
-                    number_of_islands += 1
+                if is_unvisited_land(row, col):
+                    num_islands += 1
 
                     queue = collections.deque([(row, col)])
                     seen.add((row, col))
@@ -45,8 +42,8 @@ class Solution:
                         for dr, dc in directions:
                             nr = r + dr
                             nc = c + dc
-                            if is_land(nr, nc) and (nr, nc) not in seen:
+                            if is_unvisited_land(nr, nc):
                                 queue.append((nr, nc))
                                 seen.add((nr, nc))
 
-        return number_of_islands
+        return num_islands
