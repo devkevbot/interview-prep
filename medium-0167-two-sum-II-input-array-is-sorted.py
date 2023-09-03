4a7ffe4dd8dd989e8f876e1b69d4d4932b1eeba8
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2023-09-03 12:33:03 -0700

    refine: 0167

--- medium-0167-two-sum-II-input-array-is-sorted.py
+++ medium-0167-two-sum-II-input-array-is-sorted.py
@@ -1,22 +1,20 @@
-from typing import List
-
-
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    @staticmethod
+    def two_sum(numbers: list[int], target: int) -> list[int]:
         """
         Time: O(n), where n is the input length
         Space: O(1)
         """
-        l = 0
-        r = len(numbers) - 1
+        left = 0
+        right = len(numbers) - 1
 
-        while l < r:
-            curr_sum = numbers[l] + numbers[r]
+        while left < right:
+            total = numbers[left] + numbers[right]
 
-            if target == curr_sum:
-                return [l + 1, r + 1]
+            if target == total:
+                return [left + 1, right + 1]
 
-            if target > curr_sum:
-                l += 1
+            if total < target:
+                left += 1
             else:
-                r -= 1
+                right -= 1
