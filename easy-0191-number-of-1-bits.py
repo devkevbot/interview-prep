c12b38445a1096fe6adfac35b10af3b596e699e9
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2023-09-22 14:09:11 -0700

    refine: 0191

--- easy-0191-number-of-1-bits.py
+++ easy-0191-number-of-1-bits.py
@@ -1,40 +1,26 @@
-# Bit maniuplation #1
-class Solution:
-    def hammingWeight(self, n: int) -> int:
+class BitShiftSolution:
+    @staticmethod
+    def hamming_weight(n: int) -> int:
         """
-        Time: O(n) where n is the number of bits
+        Let b = the number of bits in the input
+        Time: O(b) where b is the number of bits
         Space: O(1)
         """
-        num_bits = 0
+        num_ones = 0
 
         while n > 0:
-            num_bits += n & 1
-            n //= 2
-
-        return num_bits
-
-
-# Bit maniuplation #2
-class Solution:
-    def hammingWeight(self, n: int) -> int:
-        """
-        Time: O(n) where n is the number of bits
-        Space: O(1)
-        """
-        num_bits = 0
-
-        while n > 0:
-            num_bits += n & 1
+            num_ones += n & 1
             n >>= 1
 
-        return num_bits
+        return num_ones
 
 
-# Python built-in
-class Solution:
-    def hammingWeight(self, n: int) -> int:
+class BuiltInSolution:
+    @staticmethod
+    def hamming_weight(n: int) -> int:
         """
-        Time: O(n) where n is the number of bits
-        Space: O(n) where n is the number of bits
+        Let b = the number of bits in the input
+        Time: O(b)
+        Space: O(b)
         """
         return bin(n).count("1")
