af29ff88f279fb609236afc10082e2b9dcdbe753
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2023-09-22 13:56:36 -0700

    refine: 0070

--- easy-0070-climbing-stairs.py
+++ easy-0070-climbing-stairs.py
@@ -1,32 +1,35 @@
-# Memoized dynamic programming
-class Solution:
-    def climbStairs(self, n: int) -> int:
+class MemoizedDPSolution:
+    @staticmethod
+    def climb_stairs(n: int) -> int:
         """
+        Let n = the input n
         Time: O(n)
         Space: O(1)
         """
         cache = [1, 2]
 
-        if n <= 2:
-            return n
+        if n <= 1:
+            return cache[0]
+        if n == 2:
+            return cache[1]
 
         for _ in range(3, n + 1):
-            cache[0], cache[1] = cache[1], cache[1] + cache[0]
+            cache[0], cache[1] = cache[1], cache[0] + cache[1]
 
         return cache[1]
 
 
-# Recursive
-class Solution:
-    def climbStairs(self, n: int) -> int:
+class RecursiveSolution:
+    def climb_stairs(self, n: int) -> int:
         """
-        Time: O(2^n), at each step, 2 choices can be made. N choices have to be made.
-        Space: O(n), the maximum recursion depth is n
+        Note: this solution will cause TLE
+
+        Let n = the input n
+        Time: O(2^n), at each step, 2 choices can be made and n choices have to be made.
+        Space: O(n), the maximum recursion depth is n.
         """
 
-        def climb(n: int):
-            if n <= 2:
-                return n
-            return climb(n - 1) + climb(n - 2)
+        if n <= 2:
+            return n
 
-        return climb(n)
+        return self.climb_stairs(n - 1) + self.climb_stairs(n - 2)
