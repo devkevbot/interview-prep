0b28076a44fcb149120e1efedc7db7248278d3aa
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2023-09-22 13:43:03 -0700

    refine: 0141

--- easy-0141-linked-list-cycle.py
+++ easy-0141-linked-list-cycle.py
@@ -1,6 +1,3 @@
-from typing import Optional
-
-
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
@@ -9,15 +6,14 @@ class ListNode:
 
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
+    @staticmethod
+    def has_cycle(head: ListNode | None) -> bool:
         """
+        Let n = the number of nodes in the list.
         Time: O(n) - In the worst case, all elements of the list are traversed once, plus some
         fraction of the list is cycled while fast catches up to slow to detect a cycle.
         Space: O(1) - Only constant memory is used.
         """
-        if not head:
-            return False
-
         slow = head
         fast = head
 
