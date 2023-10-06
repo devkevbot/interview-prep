f803604d3f6b6074bb111175a03af85c4a2aaa0a
devkevbot <31908183+devkevbot@users.noreply.github.com>
2023-10-06 14:35:01 -0700

    refine: 0572

--- easy-0572-subtree-of-another-tree.py
+++ easy-0572-subtree-of-another-tree.py
@@ -1,6 +1,3 @@
-from typing import Optional
-
-
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -10,26 +7,26 @@ class TreeNode:
 
 
 class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    def is_subtree(self, root: TreeNode | None, sub_root: TreeNode | None) -> bool:
         """
-        Time: O(n * m), where n is the number of nodes in the full tree and m is the number of nodes in the subroot
-        Space: O(h), where h is the minimum of height of the full tree and the subroot tree
+        Time: O(n * m), where n is the number of nodes in the full tree and m is the number of nodes in the sub root
+        Space: O(h), where h is the minimum of height of the full tree and the sub root tree
         """
         # If the full tree is empty, nothing can be its subtree
         if root is None:
             return False
 
         # A null node is always a subtree
-        if subRoot is None:
+        if sub_root is None:
             return True
 
         return (
-            self.isSameTree(root, subRoot)
-            or self.isSubtree(root.left, subRoot)
-            or self.isSubtree(root.right, subRoot)
+                self.is_same_tree(root, sub_root)
+                or self.is_subtree(root.left, sub_root)
+                or self.is_subtree(root.right, sub_root)
         )
 
-    def isSameTree(self, a, b) -> bool:
+    def is_same_tree(self, a: TreeNode | None, b: TreeNode | None) -> bool:
         if not a and not b:
             return True
 
@@ -37,7 +34,7 @@ class Solution:
             return False
 
         return (
-            a.val == b.val
-            and self.isSameTree(a.left, b.left)
-            and self.isSameTree(a.right, b.right)
+                a.val == b.val
+                and self.is_same_tree(a.left, b.left)
+                and self.is_same_tree(a.right, b.right)
         )
