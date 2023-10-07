2b6c497bf5c3faf2666d274e461e87d2ed229f3f
devkevbot <31908183+devkevbot@users.noreply.github.com>
2023-10-07 01:44:25 -0700

    refine: 0053

--- medium-0053-maximum-subarray.py
+++ medium-0053-maximum-subarray.py
@@ -6,12 +6,11 @@ class Solution:
         Space: O(1)
         """
 
-        best_sum = nums[0]
+        best_sum = float("-infinity")
         curr_sum = 0
 
         for x in nums:
-            curr_sum = max(0, curr_sum)
-            curr_sum += x
+            curr_sum = max(x, curr_sum + x)
             best_sum = max(best_sum, curr_sum)
 
         return best_sum
