e2337ca0daea3ea08c855b8d4dd12965d54a57af
devkevbot <31908183+devkevbot@users.noreply.github.com>
2023-10-07 01:38:01 -0700

    refine: 0053

--- medium-0053-maximum-subarray.py
+++ medium-0053-maximum-subarray.py
@@ -1,20 +1,16 @@
-from typing import List
-
-
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+    @staticmethod
+    def max_subarray(nums: list[int]) -> int:
         """
         Time: O(n), where n is the length of the input
         Space: O(1)
         """
 
-        # This problem is easily solved by applying Kadane's algorithm.
-        max_sum = nums[0]
+        best_sum = float("-infinity")
         curr_sum = 0
 
-        for num in nums:
-            curr_sum = max(curr_sum, 0)
-            curr_sum += num
-            max_sum = max(max_sum, curr_sum)
+        for x in nums:
+            curr_sum = max(x, curr_sum + x)
+            best_sum = max(best_sum, curr_sum)
 
-        return max_sum
+        return best_sum
