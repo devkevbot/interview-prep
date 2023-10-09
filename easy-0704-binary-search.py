ccdff381df7d897e1a1cabc8866bedf288cfc299
devkevbot <31908183+devkevbot@users.noreply.github.com>
2023-10-08 21:57:39 -0700

    refine: 0704

--- easy-0704-binary-search.py
+++ easy-0704-binary-search.py
@@ -2,24 +2,24 @@ from typing import List
 
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    @staticmethod
+    def search(nums: List[int], target: int) -> int:
         """
-        Time: O(nlogn), where n is the size of nums
+        Time: O(n log n), where n is the size of nums
         Space: O(1)
         """
-        l = 0
-        r = len(nums) - 1
+        left = 0
+        right = len(nums) - 1
 
-        while l <= r:
-            # m = (l + r) // 2 also works in Python, but can lead to overflow in other languages
-            m = l + ((r - l) // 2)
+        while left <= right:
+            mid = left + ((right - left) // 2)
 
-            if target == nums[m]:
-                return m
+            if target == nums[mid]:
+                return mid
 
-            if target < nums[m]:
-                r = m - 1
+            if target < nums[mid]:
+                right = mid - 1
             else:
-                l = m + 1
+                left = mid + 1
 
         return -1
