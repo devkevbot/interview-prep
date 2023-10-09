2328b0b89a8b745718311beebbaa13cee7e02eb6
devkevbot <31908183+devkevbot@users.noreply.github.com>
2023-10-08 21:53:08 -0700

    refine: 0268

--- easy-0268-missing-number.py
+++ easy-0268-missing-number.py
@@ -1,35 +1,14 @@
-from typing import List
-
-
-# Sum approach
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
+    @staticmethod
+    def missing_number(nums: list[int]) -> int:
         """
-        Time: O(n) - must sum the input array
+        Time: O(n)
         Space: O(1)
         """
-        # Gauss's formula
+        # Gauss's formula: The sum from 1 to N is (N)(N+1)/2
         expected_sum = (len(nums) * (len(nums) + 1)) // 2
 
+        # O(n) time to compute sum
         given_sum = sum(nums)
 
         return expected_sum - given_sum
-
-
-# XOR approach
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        """
-        Time: O(n)
-        Space: O(n)
-        """
-        # Create list which contains both the given numbers and the numbers which should be present in the range.
-        # Example [3, 0, 1] + [0, 1, 2, 3]
-        all_numbers = nums + [i for i in range(len(nums) + 1)]
-
-        res = 0
-
-        for num in all_numbers:
-            res ^= num
-
-        return res
