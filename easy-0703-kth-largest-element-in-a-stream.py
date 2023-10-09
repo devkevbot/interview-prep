ff09c22679af13ca28b85999f58f540bcd0e0d2b
devkevbot <31908183+devkevbot@users.noreply.github.com>
2023-10-08 21:56:33 -0700

    refine: 0703

--- easy-0703-kth-largest-element-in-a-stream.py
+++ easy-0703-kth-largest-element-in-a-stream.py
@@ -1,11 +1,10 @@
 import heapq
-from typing import List
 
 
 class KthLargest:
-    def __init__(self, k: int, nums: List[int]):
+    def __init__(self, k: int, nums: list[int]):
         """
-        Time: O(nlogn)
+        Time: O(n log n)
         Space: O(n)
         """
         self.min_heap = nums
@@ -15,26 +14,26 @@ class KthLargest:
         heapq.heapify(self.min_heap)
 
         # Maintain a heap of size k
-        # O(n - k)
+        # At most O(n - k) iterations
         while len(self.min_heap) > k:
-            # O(log(n))
+            # O(log n)
             heapq.heappop(self.min_heap)
 
     def add(self, val: int) -> int:
         """
-        Time: O(logk)
+        Time: O(log k)
         Space: O(1)
         """
 
-        # O(log(k))
+        # O(log k)
         heapq.heappush(self.min_heap, val)
 
         # Maintain a heap of size k
         if len(self.min_heap) > self.k:
-            # O((log(k)))
+            # O(log k)
             heapq.heappop(self.min_heap)
 
         # Since heapq implements a min heap, if the heap contains k elements,
-        # then the root of the heap will the smallest element, or in the words,
+        # then the root of the heap will be the smallest element, or in other words,
         # the kth largest element.
         return self.min_heap[0]
