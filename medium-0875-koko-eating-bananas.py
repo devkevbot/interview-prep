df983cad1c1e73962d7b24f1fdc92a730bbc857a
devkevbot <31908183+devkevbot@users.noreply.github.com>
2023-10-24 16:31:38 -0700

    refine: 0875

--- medium-0875-koko-eating-bananas.py
+++ medium-0875-koko-eating-bananas.py
@@ -2,27 +2,33 @@ import math
 
 
 class Solution:
-    @staticmethod
-    def min_eating_speed(piles: list[int], h: int) -> int:
+    def min_eating_speed(self, piles: list[int], h: int) -> int:
         """
-        Time: O(log(max(piles)) * n), where n is the number of piles
+        Let n = the number of piles
+        Let m = the max of value in piles
+        Time: O(log(m) * n)
         Space: O(1)
         """
-
-        def is_fast_enough(_piles: list[int], hours_given: int, bph: int) -> bool:
-            hours_needed = 0
-            for pile in _piles:
-                hours_needed += math.ceil(pile / bph)
-            return hours_needed <= hours_given
-
         left = 1
         right = max(piles)
+        # Speed of k bananas per hour
+        k = max(piles)
 
         while left < right:
             mid = left + (right - left) // 2
-            if is_fast_enough(piles, h, mid):
+            hours = self.hours_needed_to_eat_piles(piles, mid)
+            # We have enough time and could possibly eat slower.
+            if hours <= h:
+                k = min(k, mid)
                 right = mid
+            # We don't have enough time and need to eat faster
             else:
                 left = mid + 1
 
-        return right
+        return k
+
+    def hours_needed_to_eat_piles(self, piles: list[int], speed: int) -> int:
+        hours = 0
+        for amount in piles:
+            hours += math.ceil(amount / speed)
+        return hours
