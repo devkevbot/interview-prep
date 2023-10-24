c5d1f861320dcc825ad2504da09968930f545bb7
devkevbot <31908183+devkevbot@users.noreply.github.com>
2023-10-24 15:56:38 -0700

    refine: 0074

--- medium-0074-search-a-2d-matrix.py
+++ medium-0074-search-a-2d-matrix.py
@@ -1,58 +1,42 @@
-from typing import List
-
-
 class Solution:
-    def binarySearch(self, nums: List[int], target: int) -> bool:
+    @staticmethod
+    def search_matrix(matrix: list[list[int]], target: int) -> bool:
         """
-        Time: O(logn), where n is the number of input elements
+        Let n = the number of rows
+        Let m = the number of cols
+
+        Time: O(log(n * m))
         Space: O(1)
         """
-        l = 0
-        r = len(nums) - 1
 
-        while l <= r:
-            m = l + (r - l) // 2
+        top = 0
+        bottom = len(matrix) - 1
+        mid = -1
 
-            if nums[m] == target:
-                return True
-
-            if nums[m] < target:
-                l = m + 1
-            else:
-                r = m - 1
-
-        return False
-
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        """
-        Time: O(log(m * n)), where m is the number of rows and n is the number of columns
-        Space: O(1)
-        """
-        if len(matrix) == 0:
-            return False
-
-        NUM_ROWS = len(matrix)
-
-        start_row = 0
-        end_row = NUM_ROWS - 1
-        mid_row = -1
-
-        # Step 1: Find which row to perform the binary search on to find the target.
-        # O(logm)
-        while start_row <= end_row:
-            mid_row = start_row + (end_row - start_row) // 2
-
-            if target > matrix[mid_row][-1]:
-                start_row = mid_row + 1
-            elif target < matrix[mid_row][0]:
-                end_row = mid_row - 1
+        while top <= bottom:
+            mid = top + (bottom - top) // 2
+            if target < matrix[mid][0]:
+                bottom = mid - 1
+            elif target > matrix[mid][-1]:
+                top = mid + 1
             else:
                 break
 
-        # In this condition, the value was not in the matrix since a suitable row could not be found.
-        if start_row > end_row:
+        if top > bottom:
             return False
 
-        # Step 2: Find the target within the row it should belong to.
-        # Olog(n)
-        return self.binarySearch(matrix[mid_row], target)
+        row = matrix[mid]
+
+        left = 0
+        right = len(row) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if row[mid] == target:
+                return True
+            if target < row[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return False
