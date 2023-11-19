9e017ea466e33e208c85524b75681c7c37f794bd
devkevbot <31908183+devkevbot@users.noreply.github.com>
2023-11-18 22:05:41 -0800

    problem: 0078

--- coding/solutions/medium-0078-subsets.py
+++ coding/solutions/medium-0078-subsets.py
@@ -5,23 +5,23 @@ class CascadingSolution:
         Starting from the empty subset, loop over each number in the input and
         add it to the existing subsets.
 
-        Time: O(n*2^n), where n is the length of nums
+        Let n = the length of nums
+        Time: O(n*2^n)
         Space: O(n*2^n)
         There are 2^n subsets with at most n numbers in each subset.
         """
-        output = [[]]
-
+        powerset = [[]]
         for num in nums:
-            output += [curr + [num] for curr in output]
-
-        return output
+            powerset += [curr + [num] for curr in powerset]
+        return powerset
 
 
 class BacktrackingSolution:
     @staticmethod
     def subsets(nums: list[int]) -> list[list[int]]:
         """
-        Time: O(n*2^n), where n is the length of nums
+        Let n = the length of nums
+        Time: O(n*2^n)
         Space: O(n*2^n)
         There are 2^n subsets with at most n numbers in each subset.
         """
@@ -45,4 +45,22 @@ class BacktrackingSolution:
             backtrack(i + 1)
 
         backtrack(0)
+        return powerset
+
+
+class BitwiseSolution:
+    @staticmethod
+    def subsets(nums: list[int]) -> list[list[int]]:
+        """
+        Let n = the length of nums
+        Time: O(n*2^n)
+        Space: O(n*2^n)
+        There are 2^n subsets with at most n numbers in each subset.
+        """
+        n = len(nums)
+        powerset = []
+        for i in range(2**n):
+            # Checks if the jth bit is set
+            subset = [nums[j] for j in range(n) if (i >> j) & 1]
+            powerset.append(subset)
         return powerset
