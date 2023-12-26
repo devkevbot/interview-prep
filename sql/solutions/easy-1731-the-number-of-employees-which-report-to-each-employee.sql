97e43d0e72b96fc0c03b6ebb12312fdbd89bfb15
devkevbot <31908183+devkevbot@users.noreply.github.com>
2023-12-25 17:56:17 -0800

    problem: 1731

---
+++ sql/solutions/easy-1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,13 @@
+SELECT
+    e2.employee_id,
+    e2.name,
+    COUNT(*) AS reports_count,
+    ROUND(AVG(e1.age), 0) AS average_age
+FROM
+    Employees e1
+INNER JOIN
+    Employees e2 ON e1.reports_to = e2.employee_id
+GROUP BY
+    e2.employee_id
+ORDER BY
+    e2.employee_id ASC
