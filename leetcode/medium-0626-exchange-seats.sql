7033e4f1aecf650e7f6f03a1a32f8951bccf598b
devkevbot <31908183+devkevbot@users.noreply.github.com>
2024-01-04 12:15:45 -0800

    problem: 0626

---
+++ leetcode/medium-0626-exchange-seats.sql
@@ -0,0 +1,8 @@
+SELECT
+    id,
+    CASE
+        WHEN id % 2 = 0 THEN LAG(student) OVER (ORDER BY id)
+        ELSE IFNULL(LEAD(student) OVER (ORDER BY id), student)
+    END AS "student"
+FROM
+    Seat
