d63103205a2b6fdc62220b6ca1d99ad97e18beae
devkevbot <31908183+devkevbot@users.noreply.github.com>
2024-01-10 10:47:22 -0800

    update problem: 0136

--- leetcode/easy-0136-single-number.py
+++ leetcode/easy-0136-single-number.py
@@ -1,15 +1,37 @@
-from typing import List
+from collections import defaultdict
 
 
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+class BitManipulationSolution:
+    def singleNumber(self, nums: list[int]) -> int:
         """
-        Time: O(n), where n is the length of nums
+        Let n = the length of the input `nums`
+        Time: O(n)
         Space: O(1)
         """
+
         res = 0
 
         for num in nums:
+            # Note: this only works in Python since Python expresses integers with an "infinite" number of digits
             res ^= num
 
         return res
+
+
+class DictionarySolution:
+    def singleNumber(self, nums: list[int]) -> int:
+        """
+        Let n = the length of the input `nums`
+        Time: O(n)
+        Space: O(n)
+        """
+
+        freq = defaultdict(int)
+
+        for n in nums:
+            freq[n] += 1
+        for n, f in freq.items():
+            if f == 1:
+                return n
+
+        return -1
