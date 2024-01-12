f4fe727c551d0ae49b228b62761324e776842417
devkevbot <31908183+devkevbot@users.noreply.github.com>
2024-01-11 22:47:20 -0800

    update problem: 0704

--- leetcode/easy-0704-binary-search.py
+++ leetcode/easy-0704-binary-search.py
@@ -1,25 +1,21 @@
-from typing import List
-
-
 class Solution:
-    @staticmethod
-    def search(nums: List[int], target: int) -> int:
+    def search(self, nums: list[int], target: int) -> int:
         """
-        Time: O(n log n), where n is the size of nums
+        Let n = the length of the input `nums`
+        Time: O(log n)
         Space: O(1)
         """
         left = 0
         right = len(nums) - 1
 
         while left <= right:
-            mid = left + ((right - left) // 2)
-
-            if target == nums[mid]:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
                 return mid
 
-            if target < nums[mid]:
-                right = mid - 1
-            else:
+            if nums[mid] < target:
                 left = mid + 1
+            else:
+                right = mid - 1
 
         return -1
