39a02190117f81fefa8807ce5d4a0aeefedb9bb0
devkevbot <31908183+devkevbot@users.noreply.github.com>
2024-01-11 22:49:37 -0800

    problem: 0035

---
+++ leetcode/easy-0035-search-insert-position.py
@@ -0,0 +1,21 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        """
+        Let n = the length of the input `nums`
+        Time: O(log n)
+        Space: O(1)
+        """
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
