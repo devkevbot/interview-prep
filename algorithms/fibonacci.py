e64ccc55e600cb708d48535ac1a8e3f6c406b792
devkevbot <31908183+devkevbot@users.noreply.github.com>
2024-05-31 22:12:56 -0700

    algorithms: update Fibonacci

--- algorithms/fibonacci.py
+++ algorithms/fibonacci.py
@@ -1,41 +1,63 @@
 """
-Fibonacci implement recursively and with dynamic programming.
+Fibonacci implemented recursively and with dynamic programming.
 """
 
 
-def fibonacci_recursive(n):
+def fibonacci_recursive(n: int) -> int:
     """
-    Time Complexity: O(2^n)
-    Space Complexity: O(n)
+    Let n = the index of the Fibonnaci number to calculate in the sequence
+    Time: O(2^n)
+    Space: O(n)
     """
-    if n <= 1:
-        return n
-
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
     return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
 
 
-def fibonacci_dynamic(n):
+def fibonacci_recursive_memoized(n: int, memo: dict[int, int]) -> int:
     """
-    Time Complexity: O(n)
-    Space Complexity: O(1)
+    Let n = the index of the Fibonnaci number to calculate in the sequence
+    Time: O(n)
+    Space: O(n)
+    """
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+
+    if n in memo:
+        return memo[n]
+
+    memo[n] = fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+    return memo[n]
+
+
+def fibonacci_dynamic_bottom_up(n: int) -> int:
+    """
+    Let n = the index of the Fibonnaci number to calculate in the sequence
+    Time: O(n)
+    Space: O(1)
     """
 
-    if n <= 1:
-        return n
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
 
-    out = [0, 1]
-    i = 2
-    while i <= n:
-        out[0], out[1] = out[1], (out[0] + out[1])
-        i += 1
+    values = [0, 1]
+    for i in range(2, n + 1):
+        values[0], values[1] = values[1], (values[0] + values[1])
 
-    return out[1]
+    return values[1]
+
+
+def main():
+    assert fibonacci_recursive(10) == 55
+    assert fibonacci_recursive_memoized(10, {}) == 55
+    assert fibonacci_dynamic_bottom_up(10) == 55
 
 
 if __name__ == "__main__":
-    assert fibonacci_recursive(10) == 55
-    assert fibonacci_dynamic(10) == 55
-
-    # Try this and see the speed difference
-    # fibonacci_recursive(500)
-    # fibonacci_dynamic(500)
+    main()
